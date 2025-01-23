a3a525146a7ee5a2f8af17b7bda4302ae39a0d23
PAUL ADUTWUM <pauladutwum303@gmail.com>
2025-01-22 23:16:57 -0500

    Update paulswebsite.js

--- paulswebsite.js
+++ paulswebsite.js
@@ -3,8 +3,8 @@ document.querySelectorAll('nav ul li a').forEach(link => {
     link.addEventListener('click', function (e) {
         const href = this.getAttribute('href');
         if (href.startsWith('#')) { // Only handle internal anchor links
-            e.preventDefault(); // Prevent default scrolling behavior
-            const targetId = href.substring(1); // Remove the '#' from href
+            e.preventDefault(); // 
+            const targetId = href.substring(1); 
             const targetSection = document.getElementById(targetId);
 
             if (targetSection) {
